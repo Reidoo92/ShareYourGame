d89c25bf2b7623f1294bb587462ad644f59c687b
Gilson Djassan <gilsondjassan@gmail.com>
2023-11-22 11:30:47 +0100

    nouvelle navbar sans les liens

--- app/controllers/video_games_controller.rb
+++ app/controllers/video_games_controller.rb
@@ -1,2 +1,41 @@
 class VideoGamesController < ApplicationController
+  # before_action :set_video_game, only: %i[ show edit update destroy ]
+  def index
+    @video_games = VideoGame.all
+  end
+  
+  def create
+    @video_game = VideoGame.new(video_game_params)
+    if @video_game.save
+      redirect_to @users, notice: "Your game was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def new
+    @video_game = VideoGame.new
+  end
+  def destroy
+    @video_game = VideoGame.find(params[:id])
+    @video_game.destroy
+    redirect_to @users, notice: "Your game was successfully deleted.", status: :see_other
+  end
+  def update
+    @video_game = VideoGame.find(params[:id])
+    if @video_game.update(video_game_params)
+      redirect_to @users, notice: "Your game was successfully updated.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  def show
+    @video_game = VideoGame.find(params[:id])
+  end
 end
+private
+  def set_video_game
+    @video_game = VideoGame.find(params[:id])
+  end
+  def video_game_params
+    params.require(:video_game).permit(:title, :description, :price, :category)
+  end
--- app/models/video_game.rb
+++ app/models/video_game.rb
@@ -1,7 +1,7 @@
 class VideoGame < ApplicationRecord
   has_many :reservations
 
-  belongs_to :user, through: :reservations
+  has_many :user, through: :reservations
 
   validates :title, :descritpion, :price, :category, presence: true
   validates :title, uniqueness: true
